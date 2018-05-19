e1de4463fd2c6500b48c24949ccc1b760e2d7873
Edward Feng <edward0414@gmail.com>
2018-05-18 20:47:02 -0400

    add some comments

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ db = client['dealtap_challenge']
 #To-Do:
 #-Counter
 #	-> for auto-increment id
+#-Converter
+#   -> convert num -> URL, URL -> num
 #-Post to /index longURL convert form
 #	-> add the longURL to the collection URL (id, longURL, shortURL)
 #	-> use the id (from auto-increment) to create a shortURL address
@@ -27,9 +29,9 @@ db = client['dealtap_challenge']
 #	-> record the request info (ip, device, time)
 #	-> redirect to the longURL page by querying the db
 #
-#-Post to /index shortURL info form
+#-Get to /info/<shortURL>
 #	-> query the stats of this link
-#	-> redirect to the shortURL info page
+#	-> return a HTML page with the info
 
 def getNextSequenceValue(sequenceName):
     
@@ -44,6 +46,7 @@ def getNextSequenceValue(sequenceName):
 
 def urlValidator(url):
     #a url validator that checks if the given url gives back http status code of 2XX
+    #Not the best one but this will do...
     try: 
         resp = requests.get(url)
         print resp.status_code
@@ -57,11 +60,21 @@ def urlValidator(url):
         return False
 
 class Converter:
-
+    """
+    A converter that generates a short url using the _id of the document in the db.
+    Idea:
+    - Every time a long url is received, an auto-incremented id will be assigned to that long url.
+    - Then, with that id, use this converter to convert the number to a short string that represents 
+    the short url.
+    - Together, this id, the long url, and the short url will be saved into the db as a document.
+    """
+    
 	_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 	_base = len(_letters)
 
 	def convertToURL(self, num):
+        #convert this 10-based number to 62-based number
+        #then, use the letter to represent the 62-based number
 
 		result = ''
 
@@ -75,6 +88,8 @@ class Converter:
 		return result
 
 	def convertToNum(self, url):
+        #decode the url into a 62-based number
+        #then, convert it back to 10-based number (the id in the db)
 
 		num = 0
 
