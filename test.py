77e4f8368c02bcb13e243963e617b017d47749d7
Edward Feng <edward0414@gmail.com>
2018-05-19 01:51:50 -0400

    Adding more test cases

--- test.py
+++ test.py
@@ -1,13 +1,62 @@
 from app import app
-from flask import json, jsonify
+from flask import url_for
 from random import randint 
 import unittest
-from app import Converter
+from util import Converter
 
 
 class FlaskTestCase(unittest.TestCase):
     
+    def test_index_get(self):
+        print "there"
+        tester = app.test_client(self)
+        resp = tester.get('/')
+        assert b'<h1>Short URL Generator</h1>' in resp.data
+        assert b'<input type="longURL" placeholder="Long URL" name="longURL" class="input" required autofocus>' in resp.data
+        assert b'<input class="btn" type="submit" value="Shorten">' in resp.data
+    
+    def test_index_exist(self):
+        tester = app.test_client(self)
+        
+        #post an url that exists in the db
+        resp = tester.post('/', data=dict(
+            longURL='https://conda.io/docs/_downloads/conda-cheatsheet.pdf'
+        ))
+        assert b'This long URL exists in the database already!' in resp.data
+
+    def test_index_invalid_url(self):
+        tester = app.test_client(self)
+        resp = tester.post('/', data=dict(
+            longURL='happy.e'
+        ))
+        assert b'This URL does not seem valid. Perhaps you forgot to add http/https in front of it?' in resp.data 
+        
+    def test_short_get_exist(self):
+        tester = app.test_client(self)
+        resp = tester.get('/short/b') #espn.com
+        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.location, 'http://www.espn.com/')
+
+    def test_short_get_invalid(self):
+        tester = app.test_client(self)
+        resp = tester.get('/short/a') #invalid shortURL that is not in the db
+        assert b'Invalid shortURL is entered.' in resp.data
+        
+    def test_info_get_exist(self):
+        tester = app.test_client(self)
+        resp = tester.get('/info/b') #exist in the db
+        assert b'<th>IP</th>' in resp.data #the table appears
+        assert b'<th>Platform</th>' in resp.data
+        assert b'<th>Browser</th>' in resp.data
+        assert b'<th>Version</th>' in resp.data
+        
+    def test_info_get_invalid(self):
+        tester = app.test_client(self)
+        resp = tester.get('/info/a') #invalid shortURL that is not in the db
+        assert b'<strong>Error:</strong>' in resp.data
+        
     def converter_test_1(self):
+        print "here"
         conv = Converter()
         i = 0
         while i < 10:
@@ -23,80 +72,6 @@ class FlaskTestCase(unittest.TestCase):
         assert 'bb' == conv.convertToURL(63)
         assert 'ba' == conv.convertToURL(62)
         assert '9' == conv.convertToURL(61)
-        
-    
-    def test_index_correct(self):
-        tester = app.test_client(self)
-        resp = tester.get('/')
-        assert b'<h1>Short URL Generator</h1>' in resp.data
-        assert b'<input type="longURL" placeholder="Long URL" name="longURL" class="input" required autofocus>' in resp.data
-        assert b'<input class="btn" type="submit" value="Shorten">' in resp.data
-#        resp = tester.post('/', data=dict(
-#            longURL=''
-#        ))
-#
-#	#Test conversations endpoint correct behaviour
-#	def test_conversations_correct(self):
-#		tester = app.test_client(self)
-#		resp = tester.get('/conversations/1234') # I have written a sample conversation with this id in the db.
-#		self.assertEqual(resp.status_code, 200)
-#		data = json.loads(resp.data)
-#		self.assertEqual(data['id'], '1234')
-#
-#	#Test conversations endpoint incorrect behaviour
-#	def test_conversations_incorrect(self):
-#		tester = app.test_client(self)
-#		resp = tester.get('/conversations/234') # This conversation_id has not been used.
-#		self.assertEqual(resp.status_code, 404)
-#		data = json.loads(resp.data)
-#		self.assertEqual(data['id'], '234')
-#		self.assertEqual(data['messages'], "Error: No conversation has started with this id!")
-#
-#
-#	#Test messages endpoint correct behaviour
-#	def test_messages_correct(self):
-#		tester = app.test_client(self)
-#		req = {"conversation_id": "1234","message": "knock knock!", "sender": "edward"}
-#		resp = tester.post('/messages', data=json.dumps(req), content_type='application/json')
-#		data = json.loads(resp.data)
-#		self.assertEqual(resp.status_code, 201)
-#		self.assertTrue(data['successful'])
-#		self.assertEqual(data['message']["sender"], "edward")
-#		self.assertEqual(data['message']["message"], "knock knock!")
-#
-#
-#	#Test messages endpoint incorrect behaviour
-#	def test_messages_incorrect(self):
-#		tester = app.test_client(self)
-#		req = {"conversation_id": "1234", "sender": "edward"}
-#		resp = tester.post('/messages', data=json.dumps(req), content_type='application/json')
-#		self.assertEqual(resp.status_code, 400)
-#		data = json.loads(resp.data)
-#		self.assertEqual(data["successful"], False)
-#
-#	#Test the process of creating a new conversation
-#	def test_new_conversation(self):
-#		tester = app.test_client(self)
-#
-#		#start a new conversation
-#		x = randint(1, 9999) #theres a good chance this id hasnt been used
-#		x = str(x)
-#		req = {"conversation_id": x,"message": "Hello ShiHan!", "sender": "Edward"}
-#		resp = tester.post('/messages', data=json.dumps(req), content_type='application/json')
-#		data = json.loads(resp.data)
-#		self.assertEqual(resp.status_code, 201)
-#		self.assertTrue(data['successful'])
-#		self.assertEqual(data['message']["sender"], "Edward")
-#		self.assertEqual(data['message']["message"], "Hello ShiHan!")
-#
-#		#check the log for that conversation
-#		resp2 = tester.get('/conversations/' + x)
-#		data2 = json.loads(resp2.data)
-#		self.assertEqual(resp2.status_code, 200)
-#		self.assertEqual(data2['id'], x)
-#		self.assertEqual(data2['messages'][0]["sender"], "Edward")
-#		self.assertEqual(data2['messages'][0]["message"], "Hello ShiHan!")
-
 
 if __name__ == "__main__":
 	unittest.main()
